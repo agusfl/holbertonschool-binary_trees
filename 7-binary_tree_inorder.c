a352d4de56c09ac89fe16c70503cecfa48fd275f
agusfl <agusflom@hotmail.com>
2022-07-15 19:47:32 +0000

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,13 +1,27 @@
 #include "binary_trees.h"
 
 /**
-* DescripcionFun - Integer stored in the node
-* @parametro: Pointer to the parent node
-* @parametro: Pointer to the left child node
-* Return: description
+* binary_tree_inorder - Function that goes through a binary tree using
+* pre-order traversal (first traverse left side,then root,and then right side).
+* @tree: Pointer to the root node of the tree to traverse
+* @func: Is a pointer to a function to call for each node.
+* The value in the node must be passed as a parameter to this function.
 */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
+	/* Check if tree or func are NULL */
+	if (tree == NULL || func == NULL)
+	{
+		return; /* Base case for exit recursion */
+	}
 
+	/* Traverse left side with recursion */
+	binary_tree_inorder(tree->left, func);
+
+	/* Call func with the data value of tree (parameter 'n' of the struct) */
+	func(tree->n);
+
+	/* Traverse right side with recursion */
+	binary_tree_inorder(tree->right, func);
 }
