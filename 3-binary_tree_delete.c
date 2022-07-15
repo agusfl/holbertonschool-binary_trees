7caf07afb02e195e92a818f47337b7ef1110a03d
agusfl <agusflom@hotmail.com>
2022-07-15 18:29:38 +0000

    task 3 and 3 main file

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,13 +1,16 @@
 #include "binary_trees.h"
 
 /**
-* DescripcionFun - Integer stored in the node
-* @parametro: Pointer to the parent node
-* @parametro: Pointer to the left child node
-* Return: description
+* binary_tree_delete - Function that deletes an entire binary tree
+* @tree: pointer to the root node of the tree to delete
+* Return: If tree is NULL, do nothing
 */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-
+	if (tree == NULL) 
+		return; /* Condicion base para salir de la recursion */
+	binary_tree_delete(tree->right); /* Recursive delete for right side of tree*/
+	binary_tree_delete(tree->left); /* Recursive delete for left side of tree*/
+	free(tree); /* free */
 }
