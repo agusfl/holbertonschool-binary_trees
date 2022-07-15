8cabbef407455dcfba9d03393a73ec52a599dd83
agusfl <agusflom@hotmail.com>
2022-07-15 18:05:33 +0000

    fix typo

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	/* Check is parent is NULL */
+	/* Check if parent is NULL */
 	if (parent == NULL)
 	{
 		return (NULL);
