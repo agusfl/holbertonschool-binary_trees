65b6b797c7d11dc37cfcfae7ce99ad6f93ab7f18
Santiago Neira <santiagoneira52@gmail.com>
2022-07-15 21:30:10 +0000

    task 12 with comments

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,13 +1,23 @@
 #include "binary_trees.h"
 
 /**
-* DescripcionFun - Integer stored in the node
-* @parametro: Pointer to the parent node
-* @parametro: Pointer to the left child node
+* binary_tree_leaves - Contar la cantidad de hojas en binary tree
+* @tree: Pointer to the parent node
 * Return: description
 */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t h = 0;
 
+	if (tree == NULL)
+		return (0);
+
+	/*retorno  cuando encuentro una hoja*/ 
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/*despúes de tener ambos retornos(hojas) las sumo y retorno*/
+	h = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (h);
 }
