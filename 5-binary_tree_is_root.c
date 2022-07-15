6da06cc5dcad803a1d8426c165d712c6ba54bf42
Santiago Neira <santiagoneira52@gmail.com>
2022-07-15 18:54:56 +0000

    check if the node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,21 @@
 #include "binary_trees.h"
 
 /**
-* DescripcionFun - Integer stored in the node
-* @parametro: Pointer to the parent node
-* @parametro: Pointer to the left child node
-* Return: description
+* binary_tree_is_root - chec if a given node is a root
+* chequeamos que no tenga parent.
+* @node: es el nodo a chequear
+* Return: is NULL, return 0, root? return 1
 */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
+	if (node == NULL)
+		return (0);
 
+	/*sabemos que es el unico nodo que no tiene padre*/
+	if (node->parent == NULL)
+		return (1);
+
+	/*en cualquier otro caso*/
+	return (0);
 }
