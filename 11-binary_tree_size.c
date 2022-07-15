9962e4f2960bc4ad13e8d375269382e90730c48b
Santiago Neira <santiagoneira52@gmail.com>
2022-07-15 20:53:11 +0000

    task 11, calculate size of binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,13 +1,21 @@
 #include "binary_trees.h"
 
 /**
-* DescripcionFun - Integer stored in the node
-* @parametro: Pointer to the parent node
-* @parametro: Pointer to the left child node
-* Return: description
+* binary_tree_size - funcion para medir el tamaño de un arbol binario
+* @tree: puntero a nodo(root) del arbol a medir el tamaño.
+* Return: Tamaño del arbol o 0 si el arbol es NULL
 */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size = 0;
 
+	/*condicion de finalización de la recursión*/
+	if (!tree)
+		return (0);
+
+	/*puedo verlo como medir la altura de ambos lados y sumar*/
+	size =  (binary_tree_size(tree->left) + binary_tree_size(tree->right)) + 1;
+
+	return (size);
 }
