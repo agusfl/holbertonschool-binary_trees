53b0ddfd8e1a855e74b5d9fcc7bf32cf3922715e
agusfl <agusflom@hotmail.com>
2022-07-16 00:09:33 +0000

    fix style - betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/*retorno  cuando encuentro una hoja*/ 
+	/*retorno  cuando encuentro una hoja*/
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
