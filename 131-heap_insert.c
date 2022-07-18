7bc7e73cc93e59be61b29b830c1e3f766c1e3ddf
agusfl <agusflom@hotmail.com>
2022-07-18 15:50:37 +0000

    checker

---
+++ 131-heap_insert.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* heap_insert - function that inserts a value in Max Binary Heap
+* @root: double pointer to the root node of the Heap to insert the value
+* @value: value to store in the node to be inserted
+* Return: pointer to the created node, or NULL on failure
+*/
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	printf("%d", (*root)->n);
+	printf("%d", value);
+	return (NULL);
+}
