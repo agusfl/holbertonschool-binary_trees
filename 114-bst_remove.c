7f0791d3bc221666915b0674510c63f7544ce710
agusfl <agusflom@hotmail.com>
2022-07-18 15:27:56 +0000

    checker

---
+++ 114-bst_remove.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+* bst_remove - function that removes a node from a Binary Search Tree
+* @root: pointer to the root node of the tree where you will remove a node
+* @value: value to remove in the tree
+* Return: pointer to the new root node of the tree after removing the desired
+* value
+*/
+
+bst_t *bst_remove(bst_t *root, int value)
+{
+	printf("%d", root->n);
+	printf("%d", value);
+	return (NULL);
+}
