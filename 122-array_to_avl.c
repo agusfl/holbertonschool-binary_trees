8b72d2cb0452859853bf180463710fe83fe3eb51
agusfl <agusflom@hotmail.com>
2022-07-18 15:39:46 +0000

    checker

---
+++ 122-array_to_avl.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* array_to_avl - function that builds an AVL tree from an array
+* @array: pointer to the first element of the array to be converted
+* @size: number of element in the array
+* Return: pointer to the root node of the created AVL tree, or NULL on failure
+*/
+
+avl_t *array_to_avl(int *array, size_t size)
+{
+	printf("%d", *array);
+	printf("%ld", size);
+	return (NULL);
+}
