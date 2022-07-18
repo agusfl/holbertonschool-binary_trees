d6f222db15c041b690d167da0ac1ec05182feff6
agusfl <agusflom@hotmail.com>
2022-07-18 15:36:51 +0000

    checker

---
+++ 121-avl_insert.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+* avl_insert - function that inserts a value in an AVL Tree
+* @tree: double pointer to the root node of the AVL tree for inserting the
+* value
+* @value: value to store in the node to be inserted
+* Return: pointer to the created node, or NULL on failure
+*/
+
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	printf("%d", (*tree)->n);
+	printf("%d", value);
+	return (NULL);
+}
