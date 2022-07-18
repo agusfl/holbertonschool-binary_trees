103e28e49c011ebc8a233d67d30b3d8b89fe6420
agusfl <agusflom@hotmail.com>
2022-07-18 15:22:13 +0000

    checker

---
+++ 112-array_to_bst.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* array_to_bst - function that builds a Binary Search Tree from an array
+* @array: pointer to the first element of the array to be converted
+* @size: number of element in the array
+* Return: pointer to the root node of the created BST, or NULL on failure
+*/
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	printf("%d", (*array));
+	printf("%ld", size);
+	return (NULL);
+}
