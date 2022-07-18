ff85e44ed2d71e1014ba2ce1bb568b20aa9d6a3a
agusfl <agusflom@hotmail.com>
2022-07-18 15:25:40 +0000

    checker

---
+++ 113-bst_search.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* bst_search - function that searches for a value in a Binary Search Tree
+* @tree: pointer to the root node of the BST to search
+* @value:  value to search in the tree
+* Return: pointer to the node containing a value equals to value
+*/
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	printf("%d", tree->n);
+	printf("%d", value);
+	return (NULL);
+}
