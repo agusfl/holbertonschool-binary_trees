bc14fe19b4764f4042a637c93b4131a3503039cb
Santiago Neira <santiagoneira52@gmail.com>
2022-07-19 14:41:06 +0000

    try task 100 advanced

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -12,7 +12,20 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second)
 {
-	printf("%d", first->n);
-	printf("%d", second->n);
+	const binary_tree_t *aux1 = first, *aux2 = second;
+
+	/*cuando el nodo es descendiente de si mismo*/
+	if (second->parent == aux1)
+		return (second->parent);
+	while (aux1)
+	{
+		aux1 = aux1->parent;
+		while (aux2)
+		{
+			aux2 = aux2->parent;
+			if (aux1 == aux2)
+				return ((binary_tree_t *)aux1);
+		}
+	}
 	return (NULL);
 }
